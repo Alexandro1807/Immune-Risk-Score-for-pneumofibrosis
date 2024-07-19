01ee951418fc46a87876b241758867f0a7b34e3e
Alexander T <sanya.trusov@gmail.com>
2024-07-19 11:25:19 +0500

    Добавьте файлы проекта.

---
+++ Immune Risk Score for pneumofibrosis/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Immune_Risk_Score_for_pneumofibrosis
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
